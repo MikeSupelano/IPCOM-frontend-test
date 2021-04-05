0c16b37ad9109f119c0372e2d1911de5d26879d1
MikeSupelano <michaelsup2003@gmail.com>
2021-04-04 23:01:13 -0500

    Esqueleto componentes

---
+++ src/components/GridPartidosBlock.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="grid-partidos"></div>  
+</template>
+<script>
+export default {
+  name: "GridPartidos",
+  props: ["hero"]
+}
+</script>
+<style lang="scss"></style>
---
+++ src/components/ThumbnailBlock.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="thumbnail"></div>  
+</template>
+<script>
+export default {
+  name: "Thumbnail",
+  props: ["thumbnail", "title", "date", "id-partido"]
+}
+</script>
+<style lang="scss"></style>
