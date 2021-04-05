bee51108aba14748f7d4595af0617264f47833e2
MikeSupelano <michaelsup2003@gmail.com>
2021-04-04 23:20:31 -0500

    handle type of list

--- src/views/Lista.vue
+++ src/views/Lista.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="lista">
-    <h1>This is a lista page</h1>
+    <h1>This is a {{ tipoLista }} page</h1>
   </div>
 </template>
 <script>
 export default {
   name: 'Lista',
   components: {
+  },
+  computed: {
+    tipoLista() {
+      return this.$route.name;
+    }
   }
 }
 </script>
